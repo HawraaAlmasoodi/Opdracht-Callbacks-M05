8431e6762aa375a14ddd55b34a2b55e318dcc482
Hawraa Al Masoodi <hawraaalmasoodi@MacBook-Air-van-Hawraa.local>
2020-09-11 11:17:38 +0200

    callbacks javascript

---
+++ callbacks.js
@@ -0,0 +1,15 @@
+let huiswerkMaken = (vak, callback) => {
+    setTimeout(() => {
+        console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+        callback();
+    }, 1000);
+}
+
+
+let klaarMetHuiswerk = () => {
+    console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
+}
+
+huiswerkMaken("wiskunde", klaarMetHuiswerk);
+
+
